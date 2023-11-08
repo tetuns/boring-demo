dc948280974579cce36c90756fea15e7374edda8
tetuns <sderosa23@fed.idserve.net>
2023-11-08 12:26:43 +0100

    hi

---
+++ BoringDemo/BoringDemo/BoringDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoringDemoApp.swift
+//  BoringDemo
+//
+//  Created by Stefano De Rosa on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BoringDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoringDemo/BoringDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoringDemo
+//
+//  Created by Stefano De Rosa on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
